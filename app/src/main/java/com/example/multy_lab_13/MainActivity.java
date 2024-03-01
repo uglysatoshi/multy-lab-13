8b9aecb699a5c6bae78a0e24cdaac08eb8cab365
satoshi <uglysatoshi@ya.ru>
2024-03-01 06:21:27 +0300

    complete

--- app/src/main/java/com/example/multy_lab_13/MainActivity.java
+++ app/src/main/java/com/example/multy_lab_13/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.multy_lab_13;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.VideoView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName()+"/" + R.raw.alblack));
+        FloatingActionButton resume = findViewById(R.id.resume);
+        resume.setOnClickListener(v -> {
+            videoView.start();
+            videoView.requestFocus();
+        });
+        FloatingActionButton pause = findViewById(R.id.pause);
+        pause.setOnClickListener(v -> videoView.pause());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.show_student) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setMessage("Автор - Колтыга Валерий Евгеньевич, 3-ТИД-7");
+            dialog.setTitle("Об авторе");
+            dialog.setNeutralButton("OK", (dialog1, which) -> dialog1.dismiss());
+            dialog.setIcon(R.mipmap.ic_launcher_round);
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
